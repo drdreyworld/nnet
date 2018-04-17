1649a02cdbb82578142edc67f29a048ae344ffe0
Andrey Tikhonov <andrey.tikhonov@winlinebet.ru>
2018-04-17 17:35:59 +0300

    tests/examples for layers: activation & softmax

---
+++ layer/activation_test.go
@@ -0,0 +1,92 @@
+package layer
+
+import (
+	"testing"
+	"github.com/drdreyworld/nnet"
+)
+
+type testActivationDoubleValue struct {}
+
+func (a *testActivationDoubleValue) Forward(v float64) float64 {
+	return 2 * v
+}
+
+func (a *testActivationDoubleValue) Backward(v float64) float64 {
+	return 0.5 * v
+}
+
+func (a *testActivationDoubleValue) Serialize() string {
+	return "testActivationDoubleValue"
+}
+
+func TestActivationConstructor(t *testing.T) {
+	l := ActivationConstructor()
+	if _, ok := l.(nnet.Layer); !ok {
+		t.Error("constructor returns not Layer type")
+	}
+	if _, ok := l.(*Activation); !ok {
+		t.Error("constructor returns not Activation layer")
+	}
+}
+
+func TestActivation_Complex(t *testing.T) {
+	nnet.ActivationsRegistry["testActivationDoubleValue"] = &testActivationDoubleValue{}
+
+	l := ActivationConstructor()
+	l.Init(nnet.LayerConfig{
+		Type: LAYER_ACTIVATION,
+		Data: nnet.LayerConfigData{
+			"ActCode" : "testActivationDoubleValue",
+		},
+	})
+
+	iw, ih, id := 10, 1, 1
+	ow, oh, od := l.InitDataSizes(iw, ih, id)
+
+	if iw != ow || ih != oh || id != od {
+		t.Error("output sizes not equal input sizes")
+	}
+
+	inputs := &nnet.Data{}
+	inputs.InitVectorRandom(iw, -1, 1)
+
+	output := l.Activate(inputs)
+
+	if l.GetOutput() != output {
+		t.Error("getOutput returns other result than activate")
+	}
+
+	if output.Dims[0] != ow || output.Dims[1] != oh || output.Dims[2] != od {
+		t.Error("output dimentions mistmatch")
+	}
+
+	for i := 0; i < len(inputs.Data); i++ {
+		if output.Data[i] != 2 * inputs.Data[i] {
+			t.Error("output value is invalid")
+		}
+	}
+
+	deltas := &nnet.Data{}
+	deltas.InitVector(iw)
+	deltas.Fill(1)
+
+	gradient := l.Backprop(deltas)
+
+	for i := 0; i < iw; i++ {
+		if gradient.Data[i] != inputs.Data[i] {
+			t.Error("gradient value is invalid")
+		}
+	}
+
+	config := l.Serialize()
+
+	if config.Type != LAYER_ACTIVATION {
+		t.Error("invalid layer type in serialized config")
+	}
+
+	actCode := config.Data.String("ActCode")
+
+	if actCode != "testActivationDoubleValue" {
+		t.Error("missed ActCode in serialized config data")
+	}
+}
---
+++ layer/softmax_test.go
@@ -0,0 +1,67 @@
+package layer
+
+import (
+	"testing"
+	"github.com/drdreyworld/nnet"
+)
+
+func TestSoftmax_Complex(t *testing.T) {
+	l := SoftmaxLayerConstructor()
+	l.Init(nnet.LayerConfig{
+		Type: LAYER_SOFTMAX,
+	})
+
+	iw, ih, id := 3, 1, 1
+	ow, oh, od := l.InitDataSizes(iw, ih, id)
+
+	if iw != ow || ih != oh || id != od {
+		t.Error("output sizes not equal input sizes")
+	}
+
+	inputs := &nnet.Data{}
+	inputs.InitVector(iw)
+	inputs.Data = []float64{0.1, 0.6, 0.3}
+
+	target := inputs.CopyZero()
+	target.Data = []float64{
+		0.25838965173797984,
+		0.42601251494920570,
+		0.31559783331281430,
+	}
+
+	output := l.Activate(inputs)
+
+	if l.GetOutput() != output {
+		t.Error("getOutput returns other result than activate")
+	}
+
+	if output.Dims[0] != ow || output.Dims[1] != oh || output.Dims[2] != od {
+		t.Error("output dimentions mistmatch")
+	}
+
+	for i := 0; i < iw; i++ {
+		if output.Data[i] != target.Data[i] {
+			t.Error("invalid output value")
+		}
+	}
+
+	deltas := &nnet.Data{}
+	deltas.InitVector(iw)
+	deltas.FillRandom(-1, 1)
+
+	// backprop in softmax not change deltas because
+	// it's last layer and gradient must be equal network deltas
+	gradient := l.Backprop(deltas)
+
+	for i := 0; i < iw; i++ {
+		if gradient.Data[i] != deltas.Data[i] {
+			t.Error("gradient value is changed")
+		}
+	}
+
+	config := l.Serialize()
+
+	if config.Type != LAYER_SOFTMAX {
+		t.Error("invalid layer type in serialized config")
+	}
+}
